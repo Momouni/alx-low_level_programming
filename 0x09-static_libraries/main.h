1b811c5a526d16668a5474ec7ca41bf5b6acda46
momouni <mohamedmouni11@gmail.com>
2023-03-15 09:09:29 -0700

    libmy

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+void _putchar(void);
+void _islower(void);
+void _isalpha(void);
+void _abs(void);
+void _isupper(void);
+void _isdigit(void);
+void _strlen(void);
+void _puts(void);
+void _strcpy(void);
+void _atoi(void);
+void _strcat(void);
+void _strncat(void);
+void _strncpy(void);
+void _strcmp(void);
+void _memset(void);
+void _memcpy(void);
+void _strchr(void);
+void _strspn(void);
+void _strpbrk(void);
+void _strstr(void);
+
+#endif
